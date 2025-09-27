5c1cb0ddb8bf1f10a4c210424809caae283f6e37
Elijah Ren <elijah.ren@stonybrook.edu>
2025-09-27 16:28:41 -0400

    Encrypt Done

--- src/caesar.c
+++ src/caesar.c
@@ -6,6 +6,21 @@
  *  Feel free to use the functions that you made in strPtr.c
 */ 
 
+//Helper function for decrypting
+int isEOM(char *strPointer){
+    if(*(strPointer) == '_' &&
+       *(strPointer+1) == '_' &&
+       *(strPointer+2) == 'E' &&
+       *(strPointer+3) == 'O' &&
+       *(strPointer+4) == 'M' &&
+       *(strPointer+5) == '_' &&
+       *(strPointer+6) == '_'){
+        return 1;
+    }
+
+    return 0;
+}
+
 int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
     if(plaintext == NULL || ciphertext == NULL){
         return -2;
@@ -19,15 +34,15 @@ int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
         char newChar;
         //Cipher only if one of the following:
         if(*(plaintext+idx) >= 97 && *(plaintext+idx) <= 122){     //Lowercase Letters
-            newChar = (((*(plaintext+idx)-97) + key + idx) % 26) + 97;
+            newChar = (((*(plaintext+idx)-97) + ((key + idx) % 26))) + 97;
             counter++;
         }
         else if(*(plaintext+idx) >= 65 && *(plaintext+idx) <= 90){  //Uppercase Letters
-            newChar = (((*(plaintext+idx)-65) + key + idx) % 26) + 65;
+            newChar = (((*(plaintext+idx)-65) + ((key + idx) % 26))) + 65;
             counter++;
         }
         else if(*(plaintext+idx) >= 48 && *(plaintext+idx) <= 57){  //Numbers
-            newChar = (((*(plaintext+idx)-48) + key + (2*idx)) % 10) + 48;
+            newChar = ((*(plaintext+idx)-48) + ((key + (2*idx)) % 10)) + 48;
             counter++;
         }
         else{   //Otherwise, just keep it the same
@@ -50,6 +65,36 @@ int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
 }
 
 int decryptCaesar(const char *ciphertext, char *plaintext, int key) {
+    if(plaintext == NULL || ciphertext == NULL){
+        return -2;
+    }
+
+    int counter = 0;
+
+    //Loop through the ciphertext
+    int idx = 0;
+    for( ; isEOM(ciphertext+idx); idx++){
+        char newChar;
+        //Cipher only if one of the following:
+        if(*(ciphertext+idx) >= 97 && *(ciphertext+idx) <= 122){     //Lowercase Letters
+            newChar = (((*(ciphertext+idx)-97) - (key + idx)) % 26) + 97;
+            counter++;
+        }
+        else if(*(ciphertext+idx) >= 65 && *(ciphertext+idx) <= 90){  //Uppercase Letters
+            newChar = (((*(ciphertext+idx)-65) - (key + idx)) % 26) + 65;
+            counter++;
+        }
+        else if(*(ciphertext+idx) >= 48 && *(ciphertext+idx) <= 57){  //Numbers
+            newChar = (((*(ciphertext+idx)-48) - (key + (2*idx))) % 10) + 48;
+            counter++;
+        }
+        else{   //Otherwise, just keep it the same
+            newChar = *(ciphertext+idx);
+        }
+
+        *(plaintext+idx) = newChar;
+    }
+
 	// TODO: implement
     (void)ciphertext;
     (void)plaintext;
@@ -72,18 +117,18 @@ int main(int argc, char* argv[]){
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
     char out[64];
-    // printf("%d\n", encryptCaesar("abc",  out, 2));
-    // printf("%s\n", out);
-    // printf("%d\n", encryptCaesar("Ayb",  out, 3));
-    // printf("%s\n", out);
-    // printf("%d\n", encryptCaesar("Cse220",  out, 1));
-    // printf("%s\n", out);
-    // printf("%d\n", encryptCaesar("CS",  out, 0));
-    // printf("%s\n", out);
-    // printf("%d\n", encryptCaesar("System Fundamentals",  out, 1));
-    // printf("%s\n", out);
-    // printf("%d\n", encryptCaesar("",  out, 42));
-    // printf("%s\n", out);
+    printf("%d\n", encryptCaesar("abc",  out, 2));
+    printf("%s\n", out);
+    printf("%d\n", encryptCaesar("Ayb",  out, 3));
+    printf("%s\n", out);
+    printf("%d\n", encryptCaesar("Cse220",  out, 1));
+    printf("%s\n", out);
+    printf("%d\n", encryptCaesar("CS",  out, 0));
+    printf("%s\n", out);
+    printf("%d\n", encryptCaesar("System Fundamentals",  out, 1));
+    printf("%s\n", out);
+    printf("%d\n", encryptCaesar("",  out, 42));
+    printf("%s\n", out);
 
 	
 	/** ---------------------- **/
