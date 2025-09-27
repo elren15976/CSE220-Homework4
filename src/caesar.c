e8f9f44819a3e323f857bbbf33bd19d8cd2c0a50
Elijah Ren <elijah.ren@stonybrook.edu>
2025-09-27 18:14:28 -0400

    Finished caesar

--- src/caesar.c
+++ src/caesar.c
@@ -6,24 +6,46 @@
  *  Feel free to use the functions that you made in strPtr.c
 */ 
 
-//Helper function for decrypting
-int isEOM(char *strPointer){
-    if(*(strPointer) == '_' &&
-       *(strPointer+1) == '_' &&
-       *(strPointer+2) == 'E' &&
-       *(strPointer+3) == 'O' &&
-       *(strPointer+4) == 'M' &&
-       *(strPointer+5) == '_' &&
-       *(strPointer+6) == '_'){
-        return 1;
+//For some reason, this isn't linking correctly
+//So I just added the function here
+void strCopy(char *dest, char *src) {
+    if(src == NULL || dest == NULL){
+        return;
     }
 
-    return 0;
+    for(int idx = 0; ; idx++){
+        *(dest+idx) = *(src+idx);
+
+        if(*(src+idx) == 0){    //Copy first, then break if needed
+            break;
+        }
+    }
+}
+
+int strDiff(const char *s1, char *s2) {
+    if(s1 == NULL || s2 == NULL){
+        return -2;
+    }
+
+    for(int idx = 0; ; idx++){
+        if(*(s1+idx) != *(s2+idx)){
+            return idx;
+        }
+
+        if(*(s1+idx) == 0 || *(s2+idx) == 0){   //Compare first, then break if needed
+            break;
+        }
+    }
+    return -1;
 }
 
 int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
     if(plaintext == NULL || ciphertext == NULL){
         return -2;
+    }
+    else if(*(plaintext) == 0){
+        strCopy(ciphertext, "undefined__EOM__");
+        return 0;
     }
 
     int counter = 0;
@@ -34,15 +56,15 @@ int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
         char newChar;
         //Cipher only if one of the following:
         if(*(plaintext+idx) >= 97 && *(plaintext+idx) <= 122){     //Lowercase Letters
-            newChar = (((*(plaintext+idx)-97) + ((key + idx) % 26))) + 97;
+            newChar = (((*(plaintext+idx)-97) + key + idx) % 26) + 97;
             counter++;
         }
         else if(*(plaintext+idx) >= 65 && *(plaintext+idx) <= 90){  //Uppercase Letters
-            newChar = (((*(plaintext+idx)-65) + ((key + idx) % 26))) + 65;
+            newChar = (((*(plaintext+idx)-65) + key + idx) % 26) + 65;
             counter++;
         }
         else if(*(plaintext+idx) >= 48 && *(plaintext+idx) <= 57){  //Numbers
-            newChar = ((*(plaintext+idx)-48) + ((key + (2*idx)) % 10)) + 48;
+            newChar = (((*(plaintext+idx)-48) + key + (2*idx)) % 10) + 48;
             counter++;
         }
         else{   //Otherwise, just keep it the same
@@ -52,6 +74,7 @@ int encryptCaesar(const char *plaintext, char *ciphertext, int key) {
         *(ciphertext+idx) = newChar;
     }
 
+    //Add __EOM__ and null character
     *(ciphertext+idx) = '_';
     *(ciphertext+idx+1) = '_';
     *(ciphertext+idx+2) = 'E';
@@ -68,38 +91,47 @@ int decryptCaesar(const char *ciphertext, char *plaintext, int key) {
     if(plaintext == NULL || ciphertext == NULL){
         return -2;
     }
+    else if(strDiff(ciphertext, "undefined__EOM__") == -1){
+        strCopy(plaintext, "undefined");
+        return 0;
+    }
 
     int counter = 0;
 
     //Loop through the ciphertext
     int idx = 0;
-    for( ; isEOM(ciphertext+idx); idx++){
+    for( ; *(ciphertext+idx) != 0; idx++){
         char newChar;
         //Cipher only if one of the following:
         if(*(ciphertext+idx) >= 97 && *(ciphertext+idx) <= 122){     //Lowercase Letters
-            newChar = (((*(ciphertext+idx)-97) - (key + idx)) % 26) + 97;
+            newChar = (((*(ciphertext+idx)-122) - (key + idx)) % 26) + 122;
             counter++;
         }
         else if(*(ciphertext+idx) >= 65 && *(ciphertext+idx) <= 90){  //Uppercase Letters
-            newChar = (((*(ciphertext+idx)-65) - (key + idx)) % 26) + 65;
+            newChar = (((*(ciphertext+idx)-90) - (key + idx)) % 26) + 90;
             counter++;
         }
         else if(*(ciphertext+idx) >= 48 && *(ciphertext+idx) <= 57){  //Numbers
-            newChar = (((*(ciphertext+idx)-48) - (key + (2*idx))) % 10) + 48;
+            newChar = (((*(ciphertext+idx)-57) - (key + (2*idx))) % 10) + 57;
             counter++;
         }
         else{   //Otherwise, just keep it the same
             newChar = *(ciphertext+idx);
         }
 
+        //Check if this is end of memory
+        if(strDiff((ciphertext+idx), "__EOM__") == -1){
+            //Add null character
+            *(plaintext+idx) = 0;
+            return counter;
+        }
+
         *(plaintext+idx) = newChar;
     }
 
-	// TODO: implement
-    (void)ciphertext;
-    (void)plaintext;
-    (void)key;
-    return 0;
+    //If this is reached, that means no EOM, but
+    //null character is reached
+    return -1;
 }
 
 
@@ -116,20 +148,43 @@ int main(int argc, char* argv[]){
 	(void)argc;
 	(void)argv;
 	/** CREATE TEST CASES HERE **/
-    char out[64];
-    printf("%d\n", encryptCaesar("abc",  out, 2));
-    printf("%s\n", out);
-    printf("%d\n", encryptCaesar("Ayb",  out, 3));
-    printf("%s\n", out);
-    printf("%d\n", encryptCaesar("Cse220",  out, 1));
-    printf("%s\n", out);
-    printf("%d\n", encryptCaesar("CS",  out, 0));
-    printf("%s\n", out);
-    printf("%d\n", encryptCaesar("System Fundamentals",  out, 1));
-    printf("%s\n", out);
-    printf("%d\n", encryptCaesar("",  out, 42));
-    printf("%s\n", out);
+    // char out[64];
+    // printf("%d\n", encryptCaesar("abc",  out, 2));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("Ayb",  out, 3));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("Cse220",  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("CS",  out, 0));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("System Fundamentals",  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("",  out, 42));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar(NULL,  out, 42));
+    // printf("%s\n", out);
+    // printf("%d\n", encryptCaesar("abc",  NULL, 42));
+    // printf("%s\n", out);
 
+    // char out[64];
+    // printf("%d\n", decryptCaesar("ceg__EOM__",  out, 2));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("Dcg__EOM__",  out, 3));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("Duh911__EOM__",  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("CT__EOM__",  out, 0));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("Tavxjs Ndxomzscjrdl__EOM__",  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("no marker",  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("NULL__EOM__",  NULL, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar(NULL,  out, 1));
+    // printf("%s\n", out);
+    // printf("%d\n", decryptCaesar("undefined__EOM__",  out, 1));
+    // printf("%s\n", out);
 	
 	/** ---------------------- **/
 	return 0;
